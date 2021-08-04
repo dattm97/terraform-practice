c7ea07330ac1714d679207955314e0b6a04b9b19
dattm <dattm@payme.vn>
2021-08-04 13:22:44 +0700

    minor change

--- instance-with-rds/datasource.tf
+++ instance-with-rds/datasource.tf
@@ -9,7 +9,7 @@ data "aws_availability_zones" "available" {}
 
 # Create a data source from a shell script for provisioning the machine. The variables will be interpolated within the script.
 data "template_file" "provision" {
-  template = file("${path.module}/provision.sh")
+  template = file("${path.module}/scripts/provision.sh")
 
   vars = {
     database_endpoint = aws_db_instance.default.endpoint
